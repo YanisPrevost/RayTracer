09a19dcd09ab1b391f3c35ec32d43dbcd6d4e919
Prevost <yanis.prevost@epitech.eu>
2025-05-02 15:59:22 +0200

    uptade

--- src/Primitives/Cone/Cone.cpp
+++ src/Primitives/Cone/Cone.cpp
@@ -40,34 +40,64 @@ namespace RayTracer {
     {
         HitInfo info;
         info.hit = false;
-
         Math::Vector3D axis = direction.normalize();
         Math::Vector3D oc = ray.origin - position;
-
         double tanTheta = radius / height;
         double tanTheta2 = tanTheta * tanTheta;
         double a = ray.direction.dot(ray.direction) - (1 + tanTheta2) * pow(ray.direction.dot(axis), 2);
         double b = 2 * (oc.dot(ray.direction) - (1 + tanTheta2) * (oc.dot(axis)) * (ray.direction.dot(axis)));
         double c = oc.dot(oc) - (1 + tanTheta2) * pow(oc.dot(axis), 2);
         double discriminant = b * b - 4 * a * c;
-        if (discriminant < 0)
+        double tSide = INFINITY;
+        bool hitSide = false;
+        if (discriminant >= 0) {
+            double t1 = (-b - sqrt(discriminant)) / (2 * a);
+            double t2 = (-b + sqrt(discriminant)) / (2 * a);
+            double t = (t1 > 0) ? t1 : ((t2 > 0) ? t2 : -1);
+            if (t > 0) {
+                Math::Point3D hitPoint = ray.origin + ray.direction * t;
+                Math::Vector3D ap = hitPoint - position;
+                double heightOnCone = ap.dot(axis);
+                if (heightOnCone >= 0 && heightOnCone <= height) {
+                    tSide = t;
+                    hitSide = true;
+                }
+            }
+        }
+        double tBase = INFINITY;
+        bool hitBase = false;
+        Math::Vector3D baseNormal;
+        Math::Point3D baseCenter = position + axis * height;
+        double denom = ray.direction.dot(axis);
+        if (fabs(denom) > 1e-6) {
+            double t = (baseCenter - ray.origin).dot(axis) / denom;
+            if (t > 0) {
+                Math::Point3D hitPoint = ray.origin + ray.direction * t;
+                Math::Vector3D baseToHit = hitPoint - baseCenter;
+                double distanceFromCenter = baseToHit.length();
+                if (distanceFromCenter <= radius) {
+                    tBase = t;
+                    hitBase = true;
+                    baseNormal = axis * (-1.0);
+                }
+            }
+        }
+        if (!hitSide && !hitBase)
             return info;
-        double t1 = (-b - sqrt(discriminant)) / (2 * a);
-        double t2 = (-b + sqrt(discriminant)) / (2 * a);
-        double t = (t1 > 0) ? t1 : t2;
-        if (t < 0)
-            return info;
-        Math::Point3D hitPoint = ray.origin + ray.direction * t;
-        Math::Vector3D ap = hitPoint - position;
-        double heightOnCone = ap.dot(axis);
-        if (heightOnCone < 0 || heightOnCone > height)
-            return info;
-        info.hit = true;
-        info.distance = t;
-        info.point = hitPoint;
-        Math::Vector3D s = hitPoint - (position + axis * heightOnCone);
-        Math::Vector3D normal = (s - axis * s.dot(axis) / axis.dot(axis)).normalize();
-        info.normal = normal;
+        if (hitSide && (!hitBase || tSide <= tBase)) {
+            info.hit = true;
+            info.distance = tSide;
+            info.point = ray.origin + ray.direction * tSide;
+            Math::Vector3D ap = info.point - position;
+            double heightOnCone = ap.dot(axis);
+            Math::Vector3D s = info.point - (position + axis * heightOnCone);
+            info.normal = (s - axis * s.dot(axis) / axis.dot(axis)).normalize();
+        } else {
+            info.hit = true;
+            info.distance = tBase;
+            info.point = ray.origin + ray.direction * tBase;
+            info.normal = baseNormal;
+        }
         info.color = color;
         info.reflection = reflection;
         return info;
